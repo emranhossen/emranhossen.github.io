cf3879bcf51d424a13048f7e67403c1fdb7c8c46
emranhossen <imran04ch@gmail.com>
2021-04-18 20:13:45 +0000

    Deploying to gh-pages from  @ ce566d1876bb6f562e24143108a02c0703869ce5 🚀

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import Layout from "../components/Layout";
-
-function MyApp({ Component, pageProps }) {
-  return <Layout pageTitle="Blog" description="My Personal Blog">
-    <Component {...pageProps} />
-  </Layout>
-}
-
-export default MyApp

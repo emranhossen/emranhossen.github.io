bc062d3caa255a7e99dc951026cd27493722ca3f
emranhossen <imran04ch@gmail.com>
2021-04-19 04:19:30 +0600

    update about

--- pages/about.jsx
+++ pages/about.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const about = () => {
     return (
         <div>
-            Hello this is Emran Hossen. Software engineer from chittagong.
+            Hello this is Emran Hossen. Software engineer from chittagong. Currently working on ReactJS projects.
         </div>
     )
 }
